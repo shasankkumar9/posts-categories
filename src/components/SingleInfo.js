9ab87e159d114b853bbc6c2fe7f8bd364f9b54e4
shasankkumar9 <shasankkumar9@gmail.com>
2021-09-18 22:19:25 +0530

    design fix #1

--- src/components/SingleInfo.js
+++ src/components/SingleInfo.js
@@ -1,25 +1,43 @@
 import React from 'react'
-import './styles/SingleInfo.css'
-import { pink } from '@mui/material/colors'
+import { makeStyles } from '@mui/styles'
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 
+const useStyles = makeStyles({
+  singleItem: {
+    margin: '0.25rem',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  ids: {
+    padding: '0 20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'hotpink',
+  },
+  body: {
+    '&::first-letter': {
+      textTransform: 'uppercase',
+    },
+  },
+})
+
 function SingleInfo({ id, title, body }) {
+  const classes = useStyles()
   return (
-    <Card className='single-item'>
+    <Card className={classes.singleItem}>
       <Typography
         variant='h2'
-        color={pink[500]}
-        align='center'
-        className='ids'
+        className={classes.ids}
         sx={{ marginRight: 0, paddingRight: 0 }}
         gutterBottom>
-        {id}
+        {id}.
       </Typography>
       <CardHeader />
-      <CardContent sx={{ marginTop: 3.5, marginLeft: 0, paddingLeft: 0 }}>
+      <CardContent>
         <Typography
           variant='h5'
           className='capitalize'
@@ -27,7 +45,7 @@ function SingleInfo({ id, title, body }) {
           sx={{ marginBottom: 2 }}>
           {title}
         </Typography>
-        <Typography variant='body1' component='div' className='body'>
+        <Typography variant='body1' component='div' className={classes.body}>
           {body}
         </Typography>
       </CardContent>
