4040fd3a79e81b946437e2f408b69db005b93b4e
shasankkumar9 <shasankkumar9@gmail.com>
2021-09-19 14:27:29 +0530

    logic fix #1

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
-import { useQuery } from 'react-query'
+import React from 'react'
 import ResultList from './ResultList'
 import Error from './Error'
 import Loading from './Loading'
 
 function Display({ category, searchTerm, data, status }) {
-  if (status === 'loading') {
-    return <Loading></Loading>
-  } else if (status === 'error' || !Array.isArray(data)) {
-    return <Error />
-  } else if (status === 'success') {
-    return (
-      <ResultList data={data} category={category} searchTerm={searchTerm} />
-    )
-  }
+  return (
+    <>
+      {status === 'loading' && <Loading />}
+      {status === 'success' && Array.isArray(data) && (
+        <ResultList data={data} category={category} searchTerm={searchTerm} />
+      )}
+      {(status === 'error' || !Array.isArray(data)) && <Error />}
+    </>
+  )
 }
 
 export default Display
